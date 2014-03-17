f547cc0fb1c060ad5d30d6eb28c285cde176d99f
skwerlman <skwerlman@gmail.com>
2014-03-17 09:34:19 -0400

    Revert "Compacted code to save space; Officially 1.0"
    Last commit had high delay between keypress and response.
    No idea why.
    
    This reverts commit 76c1f1b6c7040179643f7b0201b26c85ff55adfe.

--- lgrub.sys/api/lgrub.lua
+++ lgrub.sys/api/lgrub.lua
@@ -1,37 +1,58 @@
-version='1.0-exp'
-enabled=true
+version = '0.1-exp DEV'
+enabled = true -- For OSes to see if we exist; will be true if lgrub is present, otherwise nil
+
 local shell
 local grubDir
-local cont=false
+local cont = false -- been having weird problems with public variables, had to wrap them in functions
 local tempOS
-function listOSes()
-  local dirs=fs.list(osDir)
-  local tOut={}
+
+function prepForRun(path)
+  tempOS = path
+end
+
+function getPrepped()
+  return tempOS
+end
+
+function bootNewOS(osName) -- Will set the next OS to be run. The OS must return rather than reboot.
+  newOS = osName
+  cont = true
+end
+
+function rebootOS() -- Tell the bootloader to boot again, but doesn't change the OS to be run. The OS must return rather than reboot.
+  cont = true
+end
+
+function done()
+  cont = false
+end
+
+function goAgain()
+  return cont
+end
+
+function listOSes() -- This is a public function because OSes might allow the installation of new OSes. After a new one is installed, they should run this.
+  local dirs = fs.list(osDir)
+  local tOut = {}
   local sData
-  for i,dir in ipairs(dirs) do
-    dir=osDir..'/'..dir
-    local items=fs.list(dir)
+  for i, dir in ipairs(dirs) do
+    dir = osDir..'/'..dir
+    local items = fs.list(dir)
     if fs.isDir(dir) then
-      if fs.exists(dir..'/name.grub') then
-        local file=fs.open(dir..'/name.grub','r')
-        sData=file.readLine()
+      if fs.exists(dir..'/name.grub') then -- they have declared their name
+        local file = fs.open(dir..'/name.grub', 'r')
+        sData = file.readLine() -- We get their actual name
         file.close()
-        tOut[#tOut+1]={sData,dir}
-      else
-        tOut[#tOut+1]={dir,dir}
+        tOut[#tOut+1] = { sData, dir }
+      else -- They didn't declare their name, so we assume it's the same as the directory they're in
+        tOut[#tOut+1] = { dir, dir }
       end
     end
   end
-  table.insert(tOut,{'Shutdown','Shutdown'})
+  table.insert(tOut,{'Shutdown', 'Shutdown'})
   return tOut
 end
-function bootNewOS(osName)
-  newOS=osName
-  cont=true
-end
-function rebootOS()
-  cont=true
-end
+
 function run(osName)
   term.setCursorPos(1,1)
   term.setTextColor(colors.white)
@@ -40,28 +61,16 @@ function run(osName)
   shell.run(osName)
   term.clear()
 end
-local function setShell(t)
-  shell=t
-  grubDir=shell.dir()
-  osDir=grubDir..'/os'
+
+local function setShell( t )
+  shell = t
+  grubDir = shell.dir()
+  osDir = grubDir..'/os'
 end
+
 function init(shellRef)
   setShell(shellRef or shell)
   if not shell then
     error('Must pass a shell reference at least once')
   end
-end
--- Ignore everything below here; they are only for the bootloader to access private variables
--- Using these can break stuff
-function prepForRun(path)
-tempOS=path
-end
-function getPrepped()
-return tempOS
-end
-function done()
-cont=false
-end
-function goAgain()
-return cont
 end
