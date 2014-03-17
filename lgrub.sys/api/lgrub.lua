083fe8ebdec1c6fdf4298fe453b88b20037d0144
skwerlman <skwerlman@gmail.com>
2014-03-17 07:01:28 -0400

    Base functionality complete; timer is broken

--- lgrub.sys/api/lgrub.lua
+++ lgrub.sys/api/lgrub.lua
@@ -1,50 +1,57 @@
 local shell
 
 local grubDir
-osDir = nil
-goAgain = false
-newOS = nil
-OSes = { { nil, nil } } -- must run listOSes to populate this list
-version = t
+local cont = false
+version = '0.1-exp DEV'
 
 enabled = true -- For OSes to see if we exist; will be true if lgrub is present, otherwise nil
 
 function bootNewOS(osName) -- Will set the next OS to be run. The OS must return rather than reboot.
   newOS = osName
-  goAgain = true
+  cont = true
 end
 
 function rebootOS() -- Tell the bootloader to boot again, but doesn't change the OS to be run. The OS must return rather than reboot.
-  goAgain = true
+  cont = true
 end
 
 function done()
-  goAgain = false
+  cont = false
+end
+
+function goAgain()
+  return cont
 end
 
 function listOSes() -- This is a public function because OSes might allow the installation of new OSes. After a new one is installed, they should run this.
   local dirs = fs.list(osDir)
-  local tOut
+  local tOut = {}
+  local sData
   for i, dir in ipairs(dirs) do
+    dir = osDir..'/'..dir
     local items = fs.list(dir)
     if fs.isDir(dir) then
-      if fs.exits(dir..'/name.grub') then -- they have declared their name
-        local file = fs.open(dir..'/name.grub', 'a')
-        data = file.readLine() -- We get their actual name
-        fs.close(file)
-        tOut[#t+1] = { sData, dir }
+      if fs.exists(dir..'/name.grub') then -- they have declared their name
+        local file = fs.open(dir..'/name.grub', 'r')
+        sData = file.readLine() -- We get their actual name
+        file.close()
+        tOut[#tOut+1] = { sData, dir }
       else -- They didn't declare their name, so we assume it's the same as the directory they're in
-        tOut[#t+1] = { dir, dir }
+        tOut[#tOut+1] = { dir, dir }
       end
     end
   end
-  OSes = tOut
+  table.insert(tOut,{'Shutdown', 'Shutdown'})
+  return tOut
 end
 
 function run(osName)
-  term.clear() -- We clear the screen in case the OS doesn't
-  shell.run(osDir..osName)
-  term.clear() -- We clear the screen again, if we aren't already powered off
+  term.setCursorPos(1,1)
+  term.setTextColor(colors.white)
+  term.setBackgroundColor(colors.black)
+  term.clear()
+  shell.run(osName)
+  term.clear()
 end
 
 local function setShell( t )
@@ -54,6 +61,8 @@ local function setShell( t )
 end
 
 function init(shellRef)
-  setShell(shellRef)
-  listOSes()
+  setShell(shellRef or shell)
+  if not shell then
+    error('Must pass a shell reference at least once')
+  end
 end
