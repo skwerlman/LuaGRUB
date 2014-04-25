72192e232bffde236c2e2cc225f498d2307a5b05
skwerlman <skwerlman@gmail.com>
2014-04-25 12:50:12 -0700

    Fixed wrappedFS to use getOS()

--- lgrub.sys/api/wrappedFS.lua
+++ lgrub.sys/api/wrappedFS.lua
@@ -4,44 +4,44 @@ local oldFS = fs
 
 --wrapped functions
 function list(path)
-  return oldFS.list(lgrub.thisOS..path)
+  return oldFS.list(lgrub.getOS()..path)
 end
 function exists(path)
-  return oldFS.exists(lgrub.thisOS..path)
+  return oldFS.exists(lgrub.getOS()..path)
 end
 function isDir(path)
-  return oldFS.isDir(lgrub.thisOS..path)
+  return oldFS.isDir(lgrub.getOS()..path)
 end
 function isReadOnly(path)
-  return oldFS.isReadOnly(lgrub.thisOS..path)
+  return oldFS.isReadOnly(lgrub.getOS()..path)
 end
 function getName(path)
-  return oldFS.getName(lgrub.thisOS..path)
+  return oldFS.getName(lgrub.getOS()..path)
 end
 function getDrive(path)
-  return oldFS.getDrive(lgrub.thisOS..path)
+  return oldFS.getDrive(lgrub.getOS()..path)
 end
 function getSize(path)
-  return oldFS.getSize(lgrub.thisOS..path)
+  return oldFS.getSize(lgrub.getOS()..path)
 end
 function getFreeSpace(path)
-  return oldFS.getFreeSpace(lgrub.thisOS..path)
+  return oldFS.getFreeSpace(lgrub.getOS()..path)
 end
 function makeDir(path)
-  return oldFS.makeDir(lgrub.thisOS..path)
+  return oldFS.makeDir(lgrub.getOS()..path)
 end
 function move(fromPath, toPath)
-  return oldFS.move(lgrub.thisOS..fromPath, lgrub.thisOS..toPath)
+  return oldFS.move(lgrub.getOS()..fromPath, lgrub.getOS()..toPath)
 end
 function copy(fromPath, toPath)
-  return oldFS.copy(lgrub.thisOS..fromPath, lgrub.thisOS..toPath)
+  return oldFS.copy(lgrub.getOS()..fromPath, lgrub.getOS()..toPath)
 end
 function delete(path)
-  return oldFS.delete(lgrub.thisOS..path)
+  return oldFS.delete(lgrub.getOS()..path)
 end
 function combine(basePath, localPath)
-  return oldFS.combine(lgrub.thisOS..basePath, lgrub.thisOS..localPath)
+  return oldFS.combine(lgrub.getOS()..basePath, lgrub.getOS()..localPath)
 end
 function open(path, mode)
-  return oldFS.open(lgrub.thisOS..path, mode)
+  return oldFS.open(lgrub.getOS()..path, mode)
 end
